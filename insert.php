7a4882a6689fa9e70e8fd1fe09f13f2bfadefedd
natchayp <73693791+natchayp@users.noreply.github.com>
2020-11-19 20:25:24 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,36 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITF Database Lab</title>
+
+<html>
+
+  <head>
+    <title>ITF Lab</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>  
+  </head>
+
 <body>
-<?php
-$dbconfig = include('dbconfig.php');
-$conn = mysqli_connect($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
-if (!$conn)
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>  
+  <div class="container">
+  <?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'databaselabitf.mysql.database.azure.com', 'filmzz@databaselabitf', 'film8844@', 'filmDATAB', 3306);
+if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+$sql = "INSERT INTO guestbook (name , comment , link) VALUES ('$name', '$comment', '$link')";
+
 
 if (mysqli_query($conn, $sql)) {
-    echo '<div class="container">
-            <h3>Comment has been saved in to the database successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
-         </div>';
-} else {
+    echo ('<div class="container"><h1 class="display-1">OK!!</h1></div>');
+    echo (' <form method="get" action="show.php">
+            <button type="submit" class="btn btn-success">Continue</button>
+            </form>');
+  } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+  }
   
 mysqli_close($conn);
+if ($done)
+{
+    header("Location: https://labitf2.azurewebsites.net/show.php");
+    exit();
+}
 ?>
+  </div>
 </body>
 </html>
